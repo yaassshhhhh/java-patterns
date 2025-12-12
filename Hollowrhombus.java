04258ae464c06713d33da7fad5274ae24ced7ff1
Yash Mahajan <146362840+yaassshhhhh@users.noreply.github.com>
2025-12-12 19:56:55 +0530

    Create Hollowrhombus.java

---
+++ Hollowrhombus.java
@@ -0,0 +1,27 @@
+public class hollowrhombus {
+    public static void rhombuss(int n){
+        // outer loop
+        for(int i=1;i<=n;i++){
+            // inner loop
+            // spaces
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            // stars
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+       
+        }
+    }
+    public static void main(String[] args) {
+       rhombuss(7); 
+    }
+}
+
